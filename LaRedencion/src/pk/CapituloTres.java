ca73cd28c15e1e896107189eca1ac80759d9a0d1
TomasSG <tomassanchezgrigioni98@gmail.com>
2019-07-31 10:34:06 -0300

    Decodificando mensaje con unicode UTF8

--- LaRedencion/src/pk/CapituloTres.java
+++ LaRedencion/src/pk/CapituloTres.java
@@ -12,14 +12,19 @@ public class CapituloTres {
 	public static void main(String[] args) {
 		ArrayList<String> stringNro1 = FileManager.leerArchivo(PAHT_IN_3);
 		ArrayList<String> stringNro2 = FileManager.leerArchivo(PAHT_OUT_2);
-		BigInteger nro1=new BigInteger(stringNro1.get(0));
-		BigInteger nro2=new BigInteger(stringNro2.get(0));
-		String stringNro  = nro1.add(nro2).toString();
-		int[] valores = new int[stringNro.length() / 3];
+		BigInteger nro=new BigInteger(stringNro1.get(0)).or(new BigInteger(stringNro2.get(0)));
+		String stringNro = nro.toString();
+		int[] valores = new int[stringNro.length() / 3 + 1];
 		int j = 0;
 		for (int i = 0; i < stringNro.length(); i++) {
-			String nroParcial = String.valueOf(stringNro.charAt(i)) + String.valueOf(stringNro.charAt(++i))
-					+ String.valueOf(stringNro.charAt(++i));
+			String nroParcial;
+			if(i == 0) {
+				nroParcial = "0" + String.valueOf(stringNro.charAt(i))
+				+ String.valueOf(stringNro.charAt(++i));				
+			} else {
+				nroParcial = String.valueOf(stringNro.charAt(i)) + String.valueOf(stringNro.charAt(++i))
+				+ String.valueOf(stringNro.charAt(++i));
+			}
 			valores[j] = Integer.valueOf(nroParcial);
 			j++;
 		}
@@ -27,9 +32,36 @@ public class CapituloTres {
 		for(int i = 0; i < valores.length; i++) {
 			solucion.add(Character.toString((char)valores[i]));
 		}
-		FileManager.escribirArchivo(PAHT_OUT_3, solucion);
 		for(String s: solucion) {
 			System.out.print(s);
 		}
 	}
+	
+//	ArrayList<String> stringNro1 = FileManager.leerArchivo(PAHT_IN_3);
+//	ArrayList<String> stringNro2 = FileManager.leerArchivo(PAHT_OUT_2);
+//	BigInteger nro1=new BigInteger(stringNro1.get(0));
+//	BigInteger nro2=new BigInteger(stringNro2.get(0));
+//	String stringNro  = nro1.(nro2).toString();
+//	int[] valores = new int[stringNro.length() / 3 + 1];
+//	int j = 0;
+//	for (int i = 0; i < stringNro.length(); i++) {
+//		String nroParcial;
+//		if(i == 0) {
+//			nroParcial = "0" + String.valueOf(stringNro.charAt(i))
+//			+ String.valueOf(stringNro.charAt(++i));				
+//		} else {
+//			nroParcial = String.valueOf(stringNro.charAt(i)) + String.valueOf(stringNro.charAt(++i))
+//			+ String.valueOf(stringNro.charAt(++i));
+//		}
+//		valores[j] = Integer.valueOf(nroParcial);
+//		j++;
+//	}
+//	ArrayList<String> solucion = new ArrayList<String>();
+//	for(int i = 0; i < valores.length; i++) {
+//		solucion.add(Character.toString((char)valores[i]));
+//	}
+//	FileManager.escribirArchivo(PAHT_OUT_3, solucion);
+//	for(String s: solucion) {
+//		System.out.print(s);
+//	}
 }
